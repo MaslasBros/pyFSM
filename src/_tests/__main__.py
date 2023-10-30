f13850bf343f441782f44d0429c3898899fc4ff3
Michael Evangelos Diamantis <michaeldiamantis17@gmail.com>
2023-10-30 12:37:11 +0200

    Updates in this commit
    - Moved the fsm methods into a FSM class so we can have dynamic methods.
    - Integrated the dynamic method creation feature.
    - Documented code base.

--- src/_tests/__main__.py
+++ src/_tests/__main__.py
@@ -4,53 +4,66 @@ import os
 src_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), ".."))
 sys.path.append(src_dir)
 
-from pyFsm.fsm import *
+from pyFsm.fsmLib import *
+
+fsm = FSM()
 
 #The FSM transitions
-@transition
+@fsm.transition
 def loading():
     print("Method loading")
     pass
 
-@transition
+@fsm.transition
 def aiming():
     print("Method aiming")
     pass
 
 # The FSM states
-@state
+@fsm.state
 def idle():
     print("Method idle")
     pass
 
-@state
+@fsm.state
 def fire():
     print("Method fire")
     pass
 
-@state
+@fsm.state
 def aim():
     print("Method aim")
     pass
 
-@state
+@fsm.state
 def load():
     print("Method load")
     pass
 
-@state
+@fsm.state
 def release():
     print("Method release")
     pass
 
-def _start_():
+def _start_(fsm:FSM):
     print("Saved states")
-    printStates()
+    fsm.printStates()
     print("\nSaved Transitions")
-    printTransitions()
+    fsm.printTransitions()
+
+    fsm.createTransition(idle, load, loading)
+    fsm.createTransition(idle, release)
+    fsm.createTransition(load, aim, aiming)
+    fsm.createTransition(aim, fire)
+    fsm.createTransition(fire, idle)
+
+    print("\nSaved State pairs")
+    fsm.printStatePairs()
+
+    fsm.load().fire()
 
     pass
 
 if __name__ == '__main__':
-    _start_()
+    _start_(fsm)
     pass
---
+++ src/pyFsm/fsmLib.py
@@ -0,0 +1,85 @@
+class FSM:
+    #@TODO: Integrate merparser
+    def __init__(self):
+        """
+        Constructs a FSM instance with empty states and transitions.\n
+        """
+        
+        self.states = []
+        self.transitions = []
+        self.statePairs = []
+
+        pass
+
+    def _addToStates(self, stateFunc):
+        """
+        Adds the passed state function to the internal states list.\n
+        Throws an error if the state function is already imported or registered as a state.
+        """
+
+        if stateFunc in self.states:
+            raise ValueError("Passed state " + stateFunc.__name__ + " already imported.")
+        
+        if stateFunc in self.transitions:
+            raise ValueError("Passed transition " + stateFunc.__name__ + " is registered as a state.")
+        
+        self.states.append(stateFunc)
+        setattr(self, stateFunc.__name__, stateFunc)
+
+    def _addToTransitions(self, transFunc):
+        """
+        Adds the passed transition function to the internal transitions list.\n
+        Throws an error if the transition function is already imported or registered as a state.
+        """
+
+        if transFunc in self.transitions:
+            raise ValueError("Passed transition " + transFunc.__name__ + " already imported.")
+        
+        if transFunc in self.states:
+            raise ValueError("Passed transition " + transFunc.__name__ + " is registered as a state.")
+        
+        self.transitions.append(transFunc)
+
+    def state(self, stateFunc):
+        """
+        Decorator used to add the registered method to the states list
+        """
+
+        self._addToStates(stateFunc)
+        return stateFunc
+
+    def transition(self, transFunc):
+        """
+        Decorator used to add the registered method to the transition list
+        """
+
+        self._addToTransitions(transFunc)
+        return transFunc
+
+    def createTransition(self, currentState, nextState ,transition = None):
+        """
+        Creates a new state transition which the current state transits to the next state through the passed transition.\n
+        If the passed transition is None, then the state transition is instant.
+        """
+
+        self.statePairs.append((currentState,nextState, transition))
+        pass
+
+    #region UTILS
+    def printStates(self):
+        for i in self.states:
+            print(i.__name__)
+        pass
+
+    def printTransitions(self):
+        for i in self.transitions:
+            print(i.__name__)
+        pass
+
+    def printStatePairs(self):
+        for i in self.statePairs:
+            print(i[0].__name__ + " transits to " + i[1].__name__)
+            if i[2] is not None:
+                print("-> with transition " + i[2].__name__)
+        pass
+    #endregion
