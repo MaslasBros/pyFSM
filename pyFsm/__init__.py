c41553aa59c46aaffce00b3671b9f8da40ab1293
Michael Evangelos Diamantis <michaeldiamantis17@gmail.com>
2024-03-26 14:01:18 +0200

    Cleaned up init files

--- pyFsm/__init__.py
+++ pyFsm/__init__.py
@@ -1,7 +1,1 @@
 # This directory is a Python module
-from . import pyStateGram # noqa
-from . import fsmGlobals # noqa
-from . import fsmLib # noqa
-from . import heuristics # noqa
-from . import eventHandler # noqa
-from . import mermaidHandler # noqa
--- pyfsm/_examples/__init__.py
+++ pyfsm/_examples/__init__.py
@@ -1,3 +1,1 @@
 # This directory is a Python module
-from . import merExample # noqa
-from . import simpleExample # noqa
